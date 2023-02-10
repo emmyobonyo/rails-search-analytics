bb87462507642a3804dee260152478777dde48a1
emmyobonyo <emmyobonyo@gmail.com>
2023-02-10 22:01:47 +0300

    Add test to get a new article

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,6 +1,7 @@
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
   setup do 
     @article = Article.create(title: 'Rails', body: 'Getting started with Rails', user_id: 1)
+    @user = User.create(username: 'obonyo', email: 'obonyo@gmail.com', password: 'password', most_searched_themes: ['Rails'])
   end
 
   test "should get new" do 
