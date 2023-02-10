cc59295882e4f849d09b3e6435ae7b7f00c51223
emmyobonyo <emmyobonyo@gmail.com>
2023-02-10 17:30:26 +0300

    Create an articles controller

---
+++ app/controllers/article_controller.rb
@@ -0,0 +1,62 @@
+class ArticlesController < ApplicationController
+    before_action :set_article, only: %i[ show edit update destroy ]
+  
+    def index
+      # @articles = Article.all
+      if params[:query].present?
+        @articles = Article.where("title LIKE ?", "%#{params[:query]}%")
+      else
+        @articles = Article.all
+      end
+      
+      if turbo_frame_request?
+        render 'index', locals: { articles: @articles }
+      else
+        render :index
+      end
+    end
+  
+    def show
+    end
+  
+    def new
+      @article = Article.new
+    end
+  
+    def edit
+    end
+  
+    def create
+      @article = Article.new(article_params)
+      @article.user = current_user
+      if @article.save
+        flash[:notice] = "Article was created successfully."
+        redirect_to @article
+      else
+        render 'new', status: :unprocessable_entity
+      end
+    end
+  
+    def update
+      if @article.update(article_params)
+        flash[:notice] = 'Article was updated successfully.'
+        redirect_to @article
+      else
+        render 'edit', status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      @article.destroy
+      redirect_to articles_path
+    end
+  
+    private
+      def set_article
+        @article = Article.find(params[:id])
+      end
+  
+      def article_params
+        params.require(:article).permit(:title, :body)
+      end
+  end
